6c7b26ad005e783f8fb2230e641aa7d20a89e4f6
tommie0079 <tommie0079@users.noreply.github.com>
2026-03-04 22:13:18 +0100

    Remove temp debug scripts

--- check_db.py
+++
@@ -1,8 +0,0 @@
-﻿import sqlite3
-db = sqlite3.connect('/app/data/channels.db')
-c = db.execute('SELECT status, COUNT(1) FROM videos GROUP BY status')
-for row in c.fetchall():
-    print(row)
-c2 = db.execute('SELECT title, status FROM videos')
-for row in c2.fetchall():
-    print(row)
